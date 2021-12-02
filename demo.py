90b637e3b53e0f86549fc37da26aa98a03028b8b
dbees15 <43460075+dbees15@users.noreply.github.com>
2021-12-02 13:45:24 -0800

    Added simulation time

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import networkx
 import routing
 import random
-
+from datetime import datetime
 
 def tests(graph_generator,targetnode,nodenum,max_power,sim_num):
     print(f"\n===Tests for {nodenum} node graph===")
@@ -48,18 +48,29 @@ def tests(graph_generator,targetnode,nodenum,max_power,sim_num):
 
 #==========Script Body==========
 
-sim_num = 6000    #number of times to run simulaiton
+sim_num = 10000    #number of times to run simulaiton
 max_power = 3000    #maximum power possible for a node. must be factor of 100
 graph_generator = routing.generate_graph_6  #generator for graph
 
+print(f"Iterations per test: {sim_num}")
+print(f"Maximum node power: {max_power}\n")
+
 #==========Simulation for 6 node graph==========
+t = datetime.now().timestamp()
 tests(routing.generate_graph_6,5,6,max_power,sim_num)
+print(f"\nSimulation Time: {datetime.now().timestamp()-t}")
 
 #==========Simulation for 8 node graph==========
+t1 = datetime.now().timestamp()
 tests(routing.generate_graph_8,7,8,max_power,sim_num)
+print(f"\nSimulation Time: {round(datetime.now().timestamp()-t1,2)} seconds")
 
 #==========Simulation for 10 node graph==========
+t2 = datetime.now().timestamp()
 tests(routing.generate_graph_10,9,10,max_power,sim_num)
+print(f"\nSimulation Time: {datetime.now().timestamp()-t2}")
 
 #==========Simulation for 12 node graph==========
+t3 = datetime.now().timestamp()
 tests(routing.generate_graph_12,11,12,max_power,sim_num)
+print(f"\nSimulation Time: {datetime.now().timestamp()-t3}")
