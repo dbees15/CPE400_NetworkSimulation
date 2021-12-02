0358f52e5b9aea3d9a90051f9aaae54f8b541e15
dbees15 <43460075+dbees15@users.noreply.github.com>
2021-12-02 11:47:14 -0800

    Added 12 node graph, Created test function

--- demo.py
+++ demo.py
@@ -3,140 +3,63 @@ import routing
 import random
 
 
+def tests(graph_generator,targetnode,nodenum,max_power,sim_num):
+    print(f"\n===Tests for {nodenum} node graph===")
+
+    #run sim with reroute=True
+    resultlist = [] #list of packets sent for each sim
+    rl = []
+    for i in range(0,sim_num):  #run simulation sim_num number of times
+        result,reroute_num = routing.simulate(graph_generator,max_power,True,0,targetnode,100)
+        resultlist.append(result)
+        rl.append(reroute_num)
+
+    average = sum(resultlist) / len(resultlist) #take averages of results
+    r_average = sum(rl) / len(rl)
+
+    print(f"Simulation results with reroute enabled")
+    print(f"Average: {average}")
+    print(f"Average Reroutes: {r_average}")
+
+    resultlist.clear()  #clean up
+    rl.clear()
+
+
+
+    #run sim with reroute=False
+    resultlist = [] #list of packets sent for each sim
+    rl = []
+    for i in range(0,sim_num):  #run simulation sim_num number of times
+        result,reroute_num = routing.simulate(graph_generator,max_power,False,0,targetnode,100)
+        resultlist.append(result)
+        rl.append(reroute_num)
+
+    average = sum(resultlist) / len(resultlist) #take averages of results
+    r_average = sum(rl) / len(rl)
+
+    print(f"\nSimulation results with reroute disabled")
+    print(f"Average: {average}")
+    print(f"Average Reroutes: {r_average}")
+
+    resultlist.clear()  #clean up
+    rl.clear()
+
+
+
 #==========Script Body==========
 
-sim_num = 5000    #number of times to run simulaiton
+sim_num = 6000    #number of times to run simulaiton
 max_power = 3000    #maximum power possible for a node. must be factor of 100
 graph_generator = routing.generate_graph_6  #generator for graph
 
 #==========Simulation for 6 node graph==========
-print("===Tests for 6 node graph===")
-
-#run sim with reroute=True
-resultlist = [] #list of packets sent for each sim
-rl = []
-for i in range(0,sim_num):  #run simulation sim_num number of times
-    result,reroute_num = routing.simulate(graph_generator,max_power,True,0,5,100)
-    resultlist.append(result)
-    rl.append(reroute_num)
-
-average = sum(resultlist) / len(resultlist) #take averages of results
-r_average = sum(rl) / len(rl)
-
-print(f"Simulation results with reroute enabled")
-print(f"Average: {average}")
-print(f"Average Reroutes: {r_average}")
-
-resultlist.clear()  #clean up
-rl.clear()
-
-
-
-#run sim with reroute=False
-resultlist = [] #list of packets sent for each sim
-rl = []
-for i in range(0,sim_num):  #run simulation sim_num number of times
-    result,reroute_num = routing.simulate(graph_generator,max_power,False,0,5,100)
-    resultlist.append(result)
-    rl.append(reroute_num)
-
-average = sum(resultlist) / len(resultlist) #take averages of results
-r_average = sum(rl) / len(rl)
-
-print(f"\nSimulation results with reroute disabled")
-print(f"Average: {average}")
-print(f"Average Reroutes: {r_average}")
-
-resultlist.clear()  #clean up
-rl.clear()
-
-
+tests(routing.generate_graph_6,5,6,max_power,sim_num)
 
 #==========Simulation for 8 node graph==========
-print("\n===Tests for 8 node graph===")
-
-graph_generator = routing.generate_graph_8
-
-#run sim with reroute=True
-resultlist = [] #list of packets sent for each sim
-rl = []
-for i in range(0,sim_num):  #run simulation sim_num number of times
-    result,reroute_num = routing.simulate(graph_generator,max_power,True,0,7,100)
-    resultlist.append(result)
-    rl.append(reroute_num)
-
-average = sum(resultlist) / len(resultlist) #take averages of results
-r_average = sum(rl) / len(rl)
-
-print(f"Simulation results with reroute enabled")
-print(f"Average: {average}")
-print(f"Average Reroutes: {r_average}")
-
-resultlist.clear()  #clean up
-rl.clear()
-
-
-
-#run sim with reroute=False
-resultlist = [] #list of packets sent for each sim
-rl = []
-for i in range(0,sim_num):  #run simulation sim_num number of times
-    result,reroute_num = routing.simulate(graph_generator,max_power,False,0,7,100)
-    resultlist.append(result)
-    rl.append(reroute_num)
-
-average = sum(resultlist) / len(resultlist) #take averages of results
-r_average = sum(rl) / len(rl)
-
-print(f"\nSimulation results with reroute disabled")
-print(f"Average: {average}")
-print(f"Average Reroutes: {r_average}")
-
-resultlist.clear()  #clean up
-rl.clear()
-
-
-
+tests(routing.generate_graph_8,7,8,max_power,sim_num)
 
 #==========Simulation for 10 node graph==========
-print("\n===Tests for 10 node graph===")
+tests(routing.generate_graph_10,9,10,max_power,sim_num)
 
-graph_generator = routing.generate_graph_10
-
-#run sim with reroute=True
-resultlist = [] #list of packets sent for each sim
-rl = []
-for i in range(0,sim_num):  #run simulation sim_num number of times
-    result,reroute_num = routing.simulate(graph_generator,max_power,True,0,9,100)
-    resultlist.append(result)
-    rl.append(reroute_num)
-
-average = sum(resultlist) / len(resultlist) #take averages of results
-r_average = sum(rl) / len(rl)
-
-print(f"Simulation results with reroute enabled")
-print(f"Average: {average}")
-print(f"Average Reroutes: {r_average}")
-
-resultlist.clear()  #clean up
-rl.clear()
-
-
-
-#run sim with reroute=False
-resultlist = [] #list of packets sent for each sim
-rl = []
-for i in range(0,sim_num):  #run simulation sim_num number of times
-    result,reroute_num = routing.simulate(graph_generator,max_power,False,0,9,100)
-    resultlist.append(result)
-    rl.append(reroute_num)
-
-average = sum(resultlist) / len(resultlist) #take averages of results
-r_average = sum(rl) / len(rl)
-
-print(f"\nSimulation results with reroute disabled")
-print(f"Average: {average}")
-print(f"Average Reroutes: {r_average}")
-
-resultlist.clear()  #clean up
-rl.clear()
+#==========Simulation for 12 node graph==========
+tests(routing.generate_graph_12,11,12,max_power,sim_num)
--- routing.py
+++ routing.py
@@ -104,6 +104,40 @@ def generate_graph_10(maxpower : int):
     g.add_edge(8,9)
     return g
 
+def generate_graph_12(maxpower : int):
+    maxpower/=100
+    g = nx.Graph()
+    for x in range(0,12):
+        g.add_node(x,power=(random.randint(1,maxpower))*100)
+    #add edges
+    g.add_edge(0,1)
+    g.add_edge(0,2)
+    g.add_edge(1,3)
+    g.add_edge(1,4)
+    g.add_edge(1,2)
+    g.add_edge(2,3)
+    g.add_edge(2,4)
+    g.add_edge(3,5)
+    g.add_edge(3,6)
+    g.add_edge(3,4)
+    g.add_edge(4,5)
+    g.add_edge(4,6)
+    g.add_edge(5,6)
+    g.add_edge(5,7)
+    g.add_edge(5,8)
+    g.add_edge(6,7)
+    g.add_edge(6,8)
+    g.add_edge(7,8)
+    g.add_edge(7,9)
+    g.add_edge(7,10)
+    g.add_edge(8,9)
+    g.add_edge(8,10)
+    g.add_edge(9,10)
+    g.add_edge(9,11)
+    g.add_edge(10,11)
+    return g
+
+
 def generate_graph_6(maxpower : int):
     maxpower/=100
     g = nx.Graph()
